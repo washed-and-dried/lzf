e3604e0c62e3a45097d5af38b62d078bbfd90f33
arshpsps <arshh.gill@outlook.com>
2025-02-15 18:07:08 +0530

    getSimilarityScore() + tracebackMatrix

--- src/main.c
+++ src/main.c
@@ -9,9 +9,12 @@
 
 typedef struct {
     int **matrix;
+    int **tracebackMatrix;
     size_t row_size, col_size;
     char *row_text, *col_text;
 } Matrix;
+
+enum trace { enumMatch, enumGap1, enumGap2 };
 
 // typedef struct {
 //     int val;
@@ -38,6 +41,7 @@ Matrix *newMatrix(char *row, char *col) {
     m->col_size = strlen(col) + 1;
 
     m->matrix = zeroMatrix(m->row_size, m->col_size);
+    m->tracebackMatrix = zeroMatrix(m->row_size, m->col_size);
 
     return m;
 }
@@ -50,6 +54,13 @@ void printMatrix(Matrix *m) {
         printf("\n");
     }
     printf("\n\n");
+    for (size_t i = 0; i < m->row_size; i++) {
+        for (size_t j = 0; j < m->col_size; j++) {
+            printf("%2d ", m->tracebackMatrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
 }
 
 int max(int a, int b, int c) {
@@ -57,7 +68,8 @@ int max(int a, int b, int c) {
     return max > 0 ? max : 0;
 }
 
-void smith_waterman(Matrix *m) {
+int smith_waterman(Matrix *m) {
+    int maxScore = 0;
     for (size_t i = 1; i < m->row_size; i++) {
         for (size_t j = 1; j < m->col_size; j++) {
             int match =
@@ -65,19 +77,29 @@ void smith_waterman(Matrix *m) {
                 (m->row_text[i - 1] == m->col_text[j - 1] ? MATCH : MISMATCH);
             int gap1 = m->matrix[i - 1][j] + GAP;
             int gap2 = m->matrix[i][j - 1] + GAP;
-            m->matrix[i][j] = max(match, gap1, gap2);
+            int maxVal = max(match, gap1, gap2);
+            m->tracebackMatrix[i][j] = (maxVal == match)  ? enumMatch
+                                       : (maxVal == gap1) ? enumGap1
+                                                          : enumGap2;
+            m->matrix[i][j] = maxVal;
+            maxScore = (maxVal > maxScore) ? maxVal : maxScore;
         }
     }
+    return maxScore;
+}
+
+int getSimilarityScore(char *s1, char *s2) {
+    Matrix *m = newMatrix(s1, s2); // FIXME: free(Matrix);
+    return smith_waterman(m);
 }
 
 int main() {
-    char *s1 = "GGATCGA";
-    char *s2 = "GAATTCAGTTA";
+    char *s1 = "arshpsps";
+    char *s2 = "arsh psps";
 
-    Matrix *m = newMatrix(s1, s2);
+    int score = getSimilarityScore(s1, s2);
 
-    smith_waterman(m);
-    printMatrix(m);
+    printf("%d\n", score);
 
     return 0;
 }
