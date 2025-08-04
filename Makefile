ccf0f98f621ad42ea6a3292ae7a314e23d52890d
arshpsps <arshh.gill@outlook.com>
2025-08-04 12:27:50 +0530

    cooked + makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
-main: run | builds
+run: build | builds
 	@ ./builds/out $(ss) $(d)
+
+memchk: memvar = -g -O0
+memchk: build
+	@ valgrind --leak-check=yes ./builds/out
 
 nc: ./src/nc.c | builds
 	@ gcc -Wall -o ./builds/nc ./src/nc.c -lncurses
 	@ ./builds/nc
 
-run: ./src/main.cpp | builds
+build: ./src/main.cpp | builds
 	@ g++ -Wall -Wextra -ggdb -Wl,-rpath=./lib -o ./builds/out ./src/main.cpp -Llib -l:libctring.so
 
 clean: | builds
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
-#include <algorithm>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <sys/types.h>
-#include <dirent.h>
-#include <vector>
-#include <string>
 #include "smith_waterman.c"
+#include <algorithm>
+#include <cstdlib>
+#include <dirent.h>
+#include <string>
+#include <sys/types.h>
+#include <vector>
 
 // for the TUI
-#include <unistd.h>
-#include <termios.h>
 #include <sys/wait.h>
+#include <termios.h>
+#include <unistd.h>
+
+#include <sys/ioctl.h>
 
 using namespace std;
 
 #define MAX_LEVEL 2
-#define MAX_ENTRIES 50
+#define MAX_ENTRIES 25
 
 #define QUIT CTRL('d')
 #define ESCAPE CTRL('[')
@@ -28,19 +28,23 @@ using namespace std;
 #define MOVE_UP(LINE) printf("\E[%dA", LINE);
 #define MOVE_UP_START(LINE) printf("\E[%dF", LINE);
 
-void recurse_fs(char* dirname, vector<pair<float, int>>& sorts, vector<string>& files, char* search_string, int level) {
+void recurse_fs(char *dirname, vector<pair<float, int>> &sorts,
+                vector<string> &files, char *search_string, int level) {
     struct dirent *de; // DIRectory ENtry
 
     DIR *dir = opendir(dirname);
     if (dir == NULL) {
-        // printf("failed opening directory: %s\n", dirname); // FIXME: maybe don't fail just print warning and return? (like permission denied)
+        // printf("failed opening directory: %s\n", dirname); // FIXME: maybe
+        // don't fail just print warning and return? (like permission denied)
         // exit(EXIT_FAILURE);
         closedir(dir);
         return;
     }
 
-    while ((de = readdir(dir)) != NULL) { //TODO: maybe write a dynamic arrays library (like ctring)
-        char* path = (char*) malloc(sizeof(char) * (strlen(dirname) + strlen(de->d_name) + 2));
+    while ((de = readdir(dir)) !=
+           NULL) { // TODO: maybe write a dynamic arrays library (like ctring)
+        char *path = (char *)malloc(sizeof(char) *
+                                    (strlen(dirname) + strlen(de->d_name) + 2));
         strcpy(path, dirname);
 
         if (strcmp(dirname, "/")) { // dirname is not a /
@@ -61,15 +65,17 @@ void recurse_fs(char* dirname, vector<pair<float, int>>& sorts, vector<string>& 
                 free(path);
                 continue;
             }
-            if (!strcmp(de->d_name, ".git") || !strcmp(de->d_name, ".github") || !strcmp(de->d_name, ".cache")) {
+            if (!strcmp(de->d_name, ".git") || !strcmp(de->d_name, ".github") ||
+                !strcmp(de->d_name, ".cache")) {
                 free(path);
                 continue;
             }
 
             // printf("OTHER TYPE: [%d : %s]\n", de->d_type, de->d_name);
 
-            // FIXME: THIS LEVEL THING IS BEHAVING UNDEFINED. GOTTA CHECK WHAT'S UP WITH THIS
-            if (level < MAX_LEVEL) { //FIXME:: for now limit depth to MAX_LEVEL
+            // FIXME: THIS LEVEL THING IS BEHAVING UNDEFINED. GOTTA CHECK WHAT'S
+            // UP WITH THIS
+            if (level < MAX_LEVEL) { // FIXME:: for now limit depth to MAX_LEVEL
                 // printf("NEW PATH: %s\n", path);
 
                 recurse_fs(path, sorts, files, search_string, level + 1);
@@ -86,13 +92,11 @@ void recurse_fs(char* dirname, vector<pair<float, int>>& sorts, vector<string>& 
 struct termios orig_termios;
 
 void die(const char *s) {
-  perror(s);
-  exit(1);
+    perror(s);
+    exit(1);
 }
 
-void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
 
 void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
@@ -108,11 +112,10 @@ void enableRawMode() {
 }
 
 // FIXME: MAJOR PROBLEM, WHY DOES DEFINING THEM INSIDE MAIN FREEZES EVERYTHING?
-int main(int argc, char** argv) {
-    vector<pair<float, int>> sorts;
-    vector<string> files;
+int main(int argc, char **argv) {
 
-    if (argc != 3) { // FIXME: hard coded for now to only accept the search string and directory
+    if (argc != 3) { // FIXME: hard coded for now to only accept the search
+                     // string and directory
         printf("lzf [SEARCH STRING] [DIR]\n");
 
         exit(EXIT_FAILURE);
@@ -122,95 +125,104 @@ int main(int argc, char** argv) {
     enableRawMode(); // disabled on callback
 
     char prompt[256];
-    size_t prompt_t = 0;
+    int kk = 10;
+    int prompt_t =
+        0; // NOTE: FUCKING LONG IS FUCKED, SIZE_T IS LONG UNSIGNED INT AND LONG
+           // IS FUCKED SO CANT USE SIZE_T OR LONG IN CURRENT SCOPE
     int ch;
 
     printf("\E[H\E[2J"); // clear screen
 
     // print the separator
     MOVE_DOWN_START(1)
-    printf(""); // FIXME: limit according to width
+    printf(""
+           ""
+           ""); // FIXME: limit according to width
     MOVE_UP_START(1)
 
     while (read(STDIN_FILENO, &ch, 1) == 1) {
         switch (ch) {
-            case ESCAPE:
-            case QUIT: {
-                printf("\n");
-                exit(0);
-            } break;
-            case CTRL('h'):
-            case KEY_BACKSPACE: {
-                if (prompt_t <= 0) break;
-
-                prompt_t--;
-
-                printf("\E[1D \E[1D");
-            } break;
-            case CTRL('l'): { // clear screen
-                printf("\E[H\E[2J");
-            } break;
-
-            case '\n': // ENTER
-            case '\r': { // \n and \r since its byte-by-byte input in raw mode
-                files.clear();
-                sorts.clear();
-
-                printf("\E7"); // save cursor
-                // first we move two lines down:- one for input and one for separator
-                MOVE_DOWN_START(2)
-                printf("\E[0J"); // erase old content
-                printf("\E[?25l"); // invisible cursor
-
-                prompt[prompt_t] = '\0';
-
-                // FIXME: replace with ctring when we get character append
-                char* search_string = (char*) malloc(sizeof(char) * prompt_t);
-                memcpy(search_string, prompt, prompt_t);
-
-                char* directory = argv[2];
-
-                recurse_fs(directory, sorts, files, search_string, 0);
-                printf("%zu - %zu\n", files.size(), sorts.size());
-
-                sort(sorts.begin(), sorts.end(), [&](auto& a, auto& b){
-                    return a.first > b.first;
-                });
-
-                // FIXME: MAJOR MAJOR MAJOR ISSUE.
-                // ASS SOON ASS A LOOP IS INTRODUCED. THE I/O DOESN"T WORK!!!!!!!!!!!!!!
-                int i = 0;
-                while (i < 5) {
-                    printf("HELLO\n");
-                    i++;
-                }
-
-                // print only first 50 entires
-                // for (int i = 0; i < MAX_ENTRIES && i < files.size() && i < sorts.size(); i++) {
-                //     float score = sorts[i].first;
-                //     int idx = sorts[i].second;
-
-                //     if (score == 0.0) continue; // no need to print useless entries
-
-                //     printf("%2d. %s -> %f\n", i, files[idx].c_str(), score);
-                // }
-
-                printf("\E[?25h"); // visible cursor
-                printf("\E8"); // restore cursor
-            } break;
-            default: {
-                if (!isprint(ch)) break;
-
-                if (prompt_t >= 256) {
-                    printf("%s\n", "Reached max character limit of 1024 chars for shell promt");
-                    exit(130);
-                }
-
-                prompt[prompt_t++] = ch;
-
-                printf("%c", ch);
+        case ESCAPE:
+        case QUIT: {
+            printf("\n");
+            exit(0);
+        } break;
+        case CTRL('h'):
+        case KEY_BACKSPACE: {
+            if (prompt_t <= 0)
                 break;
+
+            prompt_t--;
+
+            printf("\E[1D \E[1D");
+        } break;
+        case CTRL('l'): { // clear screen
+            printf("\E[H\E[2J");
+        } break;
+
+        case '\n':   // ENTER
+        case '\r': { // \n and \r since its byte-by-byte input in raw mode
+            vector<pair<float, int>> sorts;
+            vector<string> files;
+
+            printf("\E7"); // save cursor
+            // first we move two lines down:- one for input and one for
+            // separator
+            MOVE_DOWN_START(2)
+            // printf("\E[0J");   // erase old content
+            printf("\E[?25l"); // invisible cursor
+
+            prompt[prompt_t] = '\0';
+
+            // FIXME: replace with ctring when we get character append
+            char *search_string = (char *)malloc(sizeof(char) * prompt_t);
+            memcpy(search_string, prompt, prompt_t);
+
+            char *directory = argv[2];
+
+            recurse_fs(directory, sorts, files, search_string, 0);
+            printf("%zu - %zu\n", files.size(), sorts.size());
+
+            sort(sorts.begin(), sorts.end(),
+                 [&](auto &a, auto &b) { return a.first > b.first; });
+
+            struct winsize ws;
+            ioctl(1, TIOCGWINSZ, &ws);
+            int term_height = ws.ws_row;
+
+            printf("%d", kk);
+            kk++;
+
+            // print only first 50 entires
+            for (int i = 0;
+                 i < MAX_ENTRIES && i < files.size() && i < sorts.size(); i++) {
+                float score = sorts[i].first;
+                int idx = sorts[i].second;
+
+                if (score == 0.0)
+                    continue; // no need to print useless entries
+
+                // printf("%2d. %s -> %f\n", i, files[idx].c_str(), score);
             }
+
+            printf("\E[?25h"); // visible cursor
+            printf("\E8");     // restore cursor
+        } break;
+        default: {
+            if (!isprint(ch))
+                break;
+
+            if (prompt_t >= 256) {
+                printf("%s\n", "Reached max character limit of 1024 chars for "
+                               "shell promt");
+                exit(130);
+            }
+
+            prompt[prompt_t++] = ch;
+
+            printf("%c", ch);
+            break;
+        }
         }
     }
 }
