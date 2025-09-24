26a2673bf862844455770ad96ec2b87011db322a
bharatm29 <bharatmhes@gmail.com>
2025-09-24 19:32:15 +0530

    basic tui using tview. Can change to tcell if we want to

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,55 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
 
-	"github.com/washed-and-dried/lzf/internals/smithwaterman"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
 func main() {
-	score, traceback, matrix := smithwaterman.ComputeMatrix("darshpsps", "afrshpsps")
-	matrix.PrintMatrix()
-	fmt.Println(score)
-	fmt.Println(traceback)
+	app := tview.NewApplication()
+
+	list := tview.NewList()
+	list.ShowSecondaryText(false)
+
+	inputField := tview.NewInputField().
+		SetFieldWidth(30).
+		SetChangedFunc(func(text string) {
+			list = list.Clear().
+				AddItem("List item "+strconv.FormatInt(rand.Int63(), 10), "", '\x00', nil).
+				AddItem("List item "+strconv.FormatInt(rand.Int63(), 10), "", '\x00', nil).
+				AddItem("List item "+strconv.FormatInt(rand.Int63(), 10), "", '\x00', nil).
+				AddItem("List item "+strconv.FormatInt(rand.Int63(), 10), "", '\x00', nil)
+		})
+
+	flex := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(list, 0, 10, false).
+		AddItem(inputField, 0, 1, true)
+
+	// Ctrl + N and Ctrl + P to navigate list
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyCtrlN {
+			list.SetCurrentItem(list.GetCurrentItem() + 1)
+			return nil
+		} else if event.Key() == tcell.KeyCtrlP {
+			list.SetCurrentItem(list.GetCurrentItem() - 1)
+			return nil
+		} else if event.Key() == tcell.KeyEnter {
+			if list.GetItemCount() <= 0 {
+				return nil
+			}
+			app.Stop()
+			fmt.Println(list.GetItemText(list.GetCurrentItem()))
+			os.Exit(0)
+		}
+
+		return event
+	})
+
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
 }
